96fc4c7478e3c6a0f22c671f56e697fc0610d037
Paul Sitoh <paulwizviz@gmail.com>
2024-11-30 21:34:29 +0000

    Add reference Solidity
    * Add helloworld solidity contract.
    
    Signed-off-by: Paul Sitoh <paulwizviz@gmail.com>

--- scripts/tools.sh
+++ scripts/tools.sh
@@ -6,20 +6,12 @@ export GO_VER=1.23.3-bookworm
 export OS_VER=22.04
 export SOLC_VER=v0.8.28
 
-COMMAND=$1
+function build_tools(){
+   docker compose -f ./build/tools/builder.yaml build
+}
 
-case $COMMAND in
-    "build")
-        docker compose -f ./build/tools/builder.yaml build
-        ;;
-    "clean")
-        docker rmi -f ${GETH_TOOLS_IMAGE_NAME}
-        docker rmi -f $(docker images --filter "dangling=true" -q)
-        ;;
-    "shell")
-        docker run -it --rm $GETH_TOOLS_IMAGE_NAME /bin/bash
-        ;;
-    *)
-        echo "Usage: $0 [build | clean]"
-        ;;
-esac
+function clean_tools(){
+    docker rmi -f ${GETH_TOOLS_IMAGE_NAME}
+    docker rmi -f $(docker images --filter "dangling=true" -q)
+}
+

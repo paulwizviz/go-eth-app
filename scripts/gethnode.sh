14792f4f492256c3379f2bd9dbf27a71624f5be2
Paul Sitoh <paulwizviz@gmail.com>
2024-12-01 22:46:42 +0000

    Add local Geth dev node
    Include docker compose start script for local Geth dev node.
    
    Signed-off-by: Paul Sitoh <paulwizviz@gmail.com>

--- scripts/gethnode.sh
+++ scripts/gethnode.sh
@@ -6,6 +6,22 @@ export OS_VER=3.18
 export GO_VER=1.22.1-alpine
 
 COMMAND=$1
+SUBCOMMAND=$2
+
+function localnode(){
+    local cmd=$1
+    case $cmd in
+        "start")
+            docker compose -f ./deployment/local-dev-node.yaml up
+            ;;
+        "stop")
+            docker compose -f ./deployment/local-dev-node.yaml down
+            ;;
+        *)
+            echo "Usage: $0 local [start|stop]"
+            ;;
+    esac
+}
 
 case $COMMAND in
     "build")
@@ -14,6 +30,9 @@ case $COMMAND in
      "clean")
         docker rmi -f ${GETH_NODE_IMAGE_NAME}
         docker rmi -f $(docker images --filter "dangling=true" -q)
+        ;;
+    "local")
+        localnode $SUBCOMMAND
         ;;
     *)
         echo "Usage: $0 [build | clean]"

84dafff77981483b251d6245755843236f80619b
Paul Sitoh <2133987+paulwizviz@users.noreply.github.com>
2024-12-10 21:20:32 +0000

    Fix examples (#15)
    Update example
    
    * Consolidate wallet under a collective example.
    * Add remarks to send a contract.
    * Remove superfluous examples.
    
    ---------
    
    Signed-off-by: Paul Sitoh <paulwizviz@gmail.com>

--- examples/deploycontract/main.go
+++ examples/deploycontract/main.go
@@ -15,33 +15,35 @@ import (
 
 func main() {
 
-	// Get random dev account
 	url := "http://localhost:8545"
 
+	// Get random dev account
 	accts, err := jrpc.Accounts(context.TODO(), url, 1)
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println(accts)
 
+	// Obtain the balance of random dev account
 	bal, err := jrpc.GetBalance(context.TODO(), url, 1, accts[0], jrpc.BlockTagLATEST)
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println(bal)
 
-	// Private key from prefunded account
+	// Private key from a hardcoded Hex.
 	privateKey, err := crypto.HexToECDSA("4c0883a69102937d6231471b5dbb6204fe512961708279a8911b138d9d808759")
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// Get the address of the assigned to the private key
 	pubkey := privateKey.PublicKey
-
 	address := crypto.PubkeyToAddress(pubkey).Hex()
 	fmt.Println(address)
 
-	// Transfer ether from dev to address
+	// Transfer ether from dev account to the address associated with the hard coded
+	// private key
 	txn1 := jrpc.TxnArg{
 		To:       address,
 		From:     accts[0],
@@ -56,6 +58,7 @@ func main() {
 	}
 	fmt.Println("Txn Hash: ", txnHash)
 
+	// Extract contract compuled data
 	pwd, err := os.Getwd()
 	if err != nil {
 		log.Fatal(err)
@@ -67,24 +70,28 @@ func main() {
 	}
 	fmt.Println(content)
 
+	// Get the nounce from pending blocks
 	nonce, err := jrpc.GetTxnCount(context.TODO(), url, 1, address, jrpc.BlockTagPENDING)
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println("nonce", nonce)
 
+	// Get suggested gas price
 	gasPrice, err := jrpc.GasPrice(context.TODO(), url, 1)
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println("Gas price: ", gasPrice)
 
+	// Get Chain ID
 	chainID, err := jrpc.NetworkID(context.TODO(), url, 1)
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println("chainid ", chainID)
 
+	// Create a transaction embedding the contract content
 	txn2 := contract.CreateContractEIP1559Txn(chainID.Int64(), uint64(nonce.Int64()), big.NewInt(1_000_000_000), gasPrice, 97590, []byte(content))
 	signedTxn, err := contract.SignTransaction(txn2, uint64(chainID.Int64()), privateKey)
 	if err != nil {
@@ -96,8 +103,10 @@ func main() {
 		log.Fatal(err)
 	}
 
+	// Get signed transaction in hex string
 	s := fmt.Sprintf("0x%v", hex.EncodeToString(b))
 
+	// Send signed transaction to Dev node
 	txnHash, err = jrpc.SendRawTransaction(context.TODO(), url, 1, s)
 	if err != nil {
 		log.Fatal(err)

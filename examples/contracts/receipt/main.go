7debd7518a6de37e760665c14dfbc7e5ddf3a3f4
Paul Sitoh <paulwizviz@gmail.com>
2024-12-15 00:02:30 +0000

    Update contract operations
    Add encoding operations.
    
    Signed-off-by: Paul Sitoh <paulwizviz@gmail.com>

---
+++ examples/contracts/receipt/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"paulwizviz/go-eth-app/internal/jrpc"
+)
+
+func main() {
+
+	client := jrpc.NewDefaultClient("http://localhost:8545")
+
+	number, err := client.BlockNumber(context.TODO(), 1)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(number)
+
+	blk, err := client.GetBlockByNumber(context.TODO(), 1, fmt.Sprintf("0x%x", number), true)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var txnHash string
+	switch v := blk.Transactions.(type) {
+	case []jrpc.TxnEIP2718:
+		for _, tx := range v {
+			txnHash = tx.Hash
+			fmt.Println(txnHash)
+		}
+	}
+
+	receipt, err := client.GetTxnReceipt(context.TODO(), 1, txnHash)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if receipt.ContractAddress == "" {
+		log.Fatal("No contract address")
+	}
+
+	fmt.Println("-->", receipt.ContractAddress, len(receipt.ContractAddress))
+}
